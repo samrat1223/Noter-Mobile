3d8f71a2f820f553118e66d6938d2c2c2c44debe
samrat1223 <samratganguly37@gmail.com>
2021-01-16 20:27:47 +0530

    Database helper class created and final implementation done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:my_noter/screens/note_details.dart';
 import 'package:my_noter/screens/note_list.dart';
+import 'package:my_noter/screens/note_detail.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return MaterialApp(
-      title: 'Notekeeper',
+      title: 'NoteKeeper',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple
+          primarySwatch: Colors.deepPurple
       ),
       home: NoteList(),
     );
--- lib/models/note.dart
+++ lib/models/note.dart
@@ -6,9 +6,9 @@ class Note {
   String _date;
   int _priority;
 
-  Note(this._title,this._date,this._priority,[this._description]);
+  Note(this._title, this._date, this._priority, [this._description]);
 
-  Note.withId(this._id,this._title,this._date,this._priority,[this._description]);
+  Note.withId(this._id, this._title, this._date, this._priority, [this._description]);
 
   int get id => _id;
 
@@ -20,56 +20,53 @@ class Note {
 
   String get date => _date;
 
-
-  set title(String newTitle){
-    if(newTitle.length<=255) {
-      this.title = newTitle ;
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
     }
   }
 
-  set description(String newDescription)
-  {
-    if(newDescription.length<=255)
-      {
-        this._description = newDescription ;
-      }
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      this._description = newDescription;
+    }
   }
 
-  set priority(int newPriority){
-    if(newPriority >= 1 && newPriority <= 2 ){
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
       this._priority = newPriority;
     }
   }
 
-  set date(String newDate){
+  set date(String newDate) {
     this._date = newDate;
   }
 
-  //Create a map object to note object
+  // Convert a Note object into a Map object
+  Map<String, dynamic> toMap() {
 
-  Map<String , dynamic> toMap() {
-
-    var map = Map<String , dynamic>();
-
-    if(id!= null) {
+    var map = Map<String, dynamic>();
+    if (id != null) {
       map['id'] = _id;
     }
     map['title'] = _title;
     map['description'] = _description;
     map['priority'] = _priority;
-    map['date']=_date;
+    map['date'] = _date;
 
-    return map ;
+    return map;
   }
 
-  // Extract a note object from a Map object
-
-  Note.fromMapObject(Map<String , dynamic> map){
-    this._id=map['id'];
-    this._priority=map['priority'];
-    this._title=map['title'];
-    this._description=map['description'];
-    this._date=map['date'];
+  // Extract a Note object from a Map object
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._description = map['description'];
+    this._priority = map['priority'];
+    this._date = map['date'];
   }
-
 }
+
+
+
+
